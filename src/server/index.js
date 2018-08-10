1785acabee3ab150215995058b8ed2b445bf116b
malkeet-bst <malkeet@bluestacks.com>
2018-08-10 06:08:03 +0530

    removed yarn

--- src/server/index.js
+++ src/server/index.js
@@ -1,14 +1,12 @@
-const express=require('express')
-const app=express()
-const server = require('http').server(app)
-const io = module.exports.io = require('socket.io')(server)
+var app = require('http').createServer()
+var io = module.exports.io = require('socket.io')(app)
 
 const PORT = process.env.PORT || 3231
 
 const SocketManager = require('./SocketManager')
-app.use(express.static(__dirname+'/../../build'))
+
 io.on('connection', SocketManager)
 
-server.listen(PORT, ()=>{
+app.listen(PORT, ()=>{
 	console.log("Connected to port:" + PORT);
 })
